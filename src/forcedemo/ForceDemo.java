f015ce03204c0a7fc90b43a8f8253b692960617b
SnowGRH <gergelyovinczeff@gmail.com>
2023-09-25 15:14:56 +0200

    while+ for

--- src/forcedemo/ForceDemo.java
+++ src/forcedemo/ForceDemo.java
@@ -16,7 +16,9 @@ public class ForceDemo {
     public static void main(String[] args) {
 
         while(true) {
-
+            for (int i = 0; i < 10; i++) {
+                
+            }
             
         }
     }
