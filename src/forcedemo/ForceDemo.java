959856ed9417655c1da8198cf925386ea6695933
SnowGRH <gergelyovinczeff@gmail.com>
2023-09-25 15:04:59 +0200

    ciklus

---
+++ src/forcedemo/ForceDemo.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package forcedemo;
+
+/**
+ *
+ * @author vinczeffy.gergely
+ */
+public class ForceDemo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        if (true) {
+            
+        }
+    }
+    
+}
